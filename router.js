1dc4bb53745f3414cda717ae175c1b45dd7b1010
NAGZYY <68570686+NAGZYY@users.noreply.github.com>
2023-10-08 05:12:40 -0400

    Update 2023-10-08, 05h12

--- router.js
+++ router.js
@@ -15,62 +15,6 @@ export const API_EndPoint = async function (HttpContext) {
                 switch (HttpContext.req.method) {
                     case 'GET':
                         controller.get(HttpContext.path.id);
-                        
-                        const { op, x, y, n } = HttpContext.path.params;
-
-                        if (!op) {
-                            HttpContext.response.badRequest("L'opération 'op' est manquante.");
-                            return true;
-                        }
-        
-                        if (op === '+') {
-                            const result = controller.performMathOperation(op, x, y);
-                            HttpContext.response.json(result);
-                            return true;
-                        } else if (op === '-') {
-                            const result = controller.performMathOperation(op, x, y);
-                            HttpContext.response.json(result);
-                            return true;
-                        } else if (op === '*') {
-                            const result = controller.performMathOperation(op, x, y);
-                            HttpContext.response.json(result);
-                            return true;
-                        } else if (op === '/') {
-                            const result = controller.performMathOperation(op, x, y);
-                            HttpContext.response.json(result);
-                            return true;
-                        } else if (op === '%') {
-                            const result = controller.performMathOperation(op, x, y);
-                            HttpContext.response.json(result);
-                            return true;
-                        } else if (op === '!') {
-                            if (!n) {
-                                HttpContext.response.badRequest("Le paramètre 'n' est manquant pour l'opération '!' (factorielle).");
-                                return true;
-                            }
-                            const result = controller.performMathOperation(op, x, y, n);
-                            HttpContext.response.json(result);
-                            return true;
-                        } else if (op === 'p') {
-                            if (!n) {
-                                HttpContext.response.badRequest("Le paramètre 'n' est manquant pour l'opération 'p' (vérification de nombre premier).");
-                                return true;
-                            }
-                            const result = controller.performMathOperation(op, x, y, n);
-                            HttpContext.response.json(result);
-                            return true;
-                        } else if (op === 'np') {
-                            if (!n) {
-                                HttpContext.response.badRequest("Le paramètre 'n' est manquant pour l'opération 'np' (n-ième nombre premier).");
-                                return true;
-                            }
-                            const result = controller.performMathOperation(op, x, y, n);
-                            HttpContext.response.json(result);
-                            return true;
-                        } else {
-                            HttpContext.response.badRequest(`L'opération '${op}' n'est pas prise en charge.`);
-                            return true;
-                        }
                         return true;
                     case 'POST':
                         if (HttpContext.payload)
